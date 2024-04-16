2779b2833a6e3eb08591932780fe5828fdc09f7c
matzesoft <matzesoft@gmail.com>
2024-04-16 11:49:29 +0200

    Refactor to Svelte Kit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {getTraindata} from '$lib/db_api/db_api.js';
+</script>
+
+<main>
+    <h1>Schkapp</h1>
+
+    <button on:click={getTraindata}>
+        Get Traindata
+    </button>
+</main>
+
+<style></style>
