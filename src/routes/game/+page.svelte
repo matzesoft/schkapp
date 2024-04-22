d107c94ecde8fa5fa832df4218739dc84e65efd0
NAP-Programing <robert.becherer1@gmail.com>
2024-04-22 18:26:37 +0200

    Added Scroll function in /game

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -5,7 +5,7 @@
     import PlaceBet from "./PlaceBet.svelte";
     import EndScreen from "./EndScreen.svelte";
     $: gameStep = 0;
-
+    let y;
     function SelectTrain() {
         gameStep = 0;
     }
@@ -17,9 +17,11 @@
     }
 
 </script>
+<svelte:window bind:scrollY={y} />
 
+<div id="scroll-content">
 <main>
-    <h3>Game</h3>
+    <h3>//TODO: ADD PLAYER Game</h3>
 
     {#if gameStep === 0}
         <SelectTrains />
@@ -32,13 +34,19 @@
     {/if}
 
 </main>
+</div>
 
+//TODO: NEXT BUTTON ADD AND CENTER IT
 <button on:click={SelectTrain}>Züge Auswählen</button>
 <button on:click={placeBet}>Wette Plazieren</button>
 <button on:click={resultScreen}>Ergebnisse</button>
 <div>{@html data.text}</div>
 
 <style>
+    #scroll-content{
+        overflow-y: auto;
+        height: 90vh;
+    }
     main{
         background-color: rgba(250, 250, 250, 0.9);
         padding: 10px;
