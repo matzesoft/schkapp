16c7c579efabe8d4b2f161cc4d18ad59f03b0e01
NAP-Programing <robert.becherer1@gmail.com>
2024-05-03 23:26:21 +0200

    String geficke

--- src/lib/game_state/Schkubitrains.js
+++ src/lib/game_state/Schkubitrains.js
@@ -40,27 +40,37 @@ export class Schkubitrains {
     transformTrainData(train){
         let arrivalStartStation = typeof train.arrival.vonIrgendwoNachStation === 'string'
             ? train.arrival.vonIrgendwoNachStation.split('|')[0] + " -> Stuttgart-HBF"
-            : 'Unknown Station';
+            : null;
 
-        let name = typeof train.departure.abStationNachIrgendwo === 'string'
+        let departureEndStation = typeof train.departure.abStationNachIrgendwo === 'string'
             ? "Stuttgart-HBF -> " + train.departure.abStationNachIrgendwo.split('|').pop()
-            : 'Unknown Station';
+            : null;
 
-        let departureTime = train.departure.abfahrtAbStation ? train.departure.abfahrtAbStation : null;
-        let arrivalTime = train.arrival.ankunftInStation ? train.arrival.ankunftInStation : null;
-        let changeMessage = train.trainChanges ? train.trainChanges.changeMessage : null;
+        let test = JSON.stringify(train.trainChanges?.arrival.ankunftNachricht);
+
+        let message = train.trainChanges?.arrival.ankunftNachricht;
+
+        //if message === undefined {
+
+        for (let i = 0; i < message.length; i++) {
+            console.log(message[i].message);
+        }
+
+
+
 
         return{
             id: train.trainID,
-            nummer: train.tripLabel.zugNummer,
+            number: train.tripLabel.zugNummer,
             trainType: train.tripLabel.zugArt,
             arrivalStartStation: arrivalStartStation,
-            departureEndStation: name,
-            arrivalTime: arrivalTime,
-            departureTime: departureTime,
-            changeMessage: changeMessage,
-            newArrivalTime: JSON.stringify(train.trainChanges?.arrival),
-            newDepartureTime: JSON.stringify(train.trainChanges?.departure)
+            departureEndStation: departureEndStation,
+            message: message,
+            test : test
+            //arrivalTime: arrivalTime,
+            //departureTime: departureTime,
+            //newArrivalTime: train.trainChanges?.arrival.neueAnkunftszeit,
+            //newDepartureTime: train.trainChanges?.departure.neueAbfahrtsZeit,
         };
     }
 
