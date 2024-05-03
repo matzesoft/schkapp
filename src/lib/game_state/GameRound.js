e8ccbc23986846e368549aa20532b7e2d07208a7
matzesoft <matzesoft@gmail.com>
2024-05-03 18:22:14 +0200

    Manage step and current player

--- src/lib/game_state/GameRound.js
+++ src/lib/game_state/GameRound.js
@@ -1,4 +1,4 @@
-import {Schkubitrains} from "$lib/game_state/Schkubitrains.js";
+import {chooseBetStep, choosePlayerCountStep, chooseTrainStep, resultsStep} from "$lib/constants.js";
 
 export class GameRound {
     constructor(serialized = undefined) {
@@ -14,18 +14,44 @@ export class GameRound {
     }
 
     async createRound(trains) {
-        this.step = 0;
+        this.setStep(choosePlayerCountStep);
         this.currentPlayer = 0;
         this.trains = trains;
         this.selectedBets = [];
+
+        console.log(this.currentPlayer);
+    }
+
+    setStep(step) {
+        if (step < 0 || step > 3) {
+            throw new Error("Invalid step");
+        }
+        this.step = step;
     }
 
     setPlayerCount(playerCount) {
+        if (playerCount < 1 && playerCount > 10) {
+            throw new Error("Invalid player count");
+        }
         this.playerCount = playerCount;
+        this.setStep(chooseTrainStep);
     }
 
-    setSelectedTrain(trainId) {
+    setSelectedTrainForCurrentPlayer(trainId) {
         this.selectedBets[this.currentPlayer] = { trainId : trainId };
+        this.setStep(chooseBetStep);
+    }
+
+    setSelectedBetForCurrentPlayer(bet) {
+        // TODO: Add bet to bets array
+        //this.selectedBets[this.currentPlayer] = bet;
+
+        if (this.currentPlayer === this.playerCount-1) {
+            this.setStep(resultsStep);
+        } else {
+            this.setStep(chooseTrainStep);
+            this.currentPlayer += 1;
+        }
     }
 
     toJson() {
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -3,12 +3,13 @@
     import SelectTrains from "./SelectTrains.svelte";
     import PlaceBet from "./PlaceBet.svelte";
     import EndScreen from "./EndScreen.svelte";
+    import {choosePlayerCountStep, chooseTrainStep, chooseBetStep, resultsStep} from "$lib/constants.js";
+
     export let data;
     $: gameStep = data.step;
+    $: currentPlayer = data.currentPlayer;
     $: trains = data.trains;
     let selectetTrain = null;
-    let plyers = [{ id: 0, name: "Kevin" }, { id: 1, name: "Sven"}, { id: 2, name: "EEMA"}];
-    $: currentPlayer = plyers[gameStep].name;
 
     let y;
     function SelectTrain() {gameStep = 0;}
@@ -22,14 +23,16 @@
 <div id="scroll-content">
 <main>
 
-    {#if gameStep === 0}
+    {#if gameStep === choosePlayerCountStep}
+        <h3>ChoosePlayerCount</h3>
+    {:else if gameStep === chooseTrainStep}
         //TODO ADD PLAYER
         <h3>Current Player: {currentPlayer}</h3>
         <SelectTrains {trains} bind:selectedTrain={selectetTrain}/>
-    {:else if gameStep === 1}
+    {:else if gameStep === chooseBetStep}
         <h3>Current Player: {currentPlayer}</h3>
         <PlaceBet />
-    {:else if gameStep === 2}
+    {:else if gameStep === resultsStep}
         <h1>GAME OVER!</h1>
         <EndScreen />
     {:else}
