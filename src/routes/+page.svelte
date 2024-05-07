dd605e11fdd10869d81f174312343094e92ac622
matzesoft <matzesoft@gmail.com>
2024-05-08 00:01:29 +0200

    Add spacing to start buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <div>
-    <button class="center-button">
-        <a href="/game"> Spiel starten </a>
-    </button>
+    <span>
+        <button class="center-button">
+            <a href="/game"> Spiel starten </a>
+        </button>
+    </span>
 
-    <button class="center-button">
-        <a href="/about"> Anleitung </a>
-    </button>
+    <span>
+        <button class="center-button">
+            <a href="/about"> Anleitung </a>
+        </button>
+    </span>
 </div>
+
+<style>
+    span {
+        margin: 16px;
+    }
+</style>
