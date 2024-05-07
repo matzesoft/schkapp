746f791515ed3ff77b364ab648df45be0258e85c
matzesoft <matzesoft@gmail.com>
2024-05-08 00:08:39 +0200

    Improve about styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,24 +3,24 @@
 </script>
 
 <div>
-    <h2>Spielidee</h2>
-    <ul>
-        <li>Es wird auf Verspätungsbegründungen von Zügen gewettet. Eine Verspätungsbegründung ist zum Beispiel
+    <div>
+        <h1 style="margin-top: 0px">Spielidee</h1>
+        <p>Es wird auf Verspätungsbegründungen von Zügen gewettet. Eine Verspätungsbegründung ist zum Beispiel
             “Verspätung eines vorrausfahrenden Zuges” oder “Bauarbeiten”.
-        </li>
-        <li>Züge, auf die gewettet wird, werden live von der Deutschen Bahn angefragt und enthalten reale
+        </p>
+        <p>Züge, auf die gewettet wird, werden live von der Deutschen Bahn angefragt und enthalten reale
             Ereignisse.
-        </li>
-        <li>Es handelt sich um ein Trinkspiel, wird richtig getippt dürfen Schlücke an andere Mitspieler verteilt
+        </p>
+        <p>Es handelt sich um ein Trinkspiel, wird richtig getippt dürfen Schlücke an andere Mitspieler verteilt
             werden. Wird falsch getippt muss man selbst trinken.
-        </li>
-    </ul>
+        </p>
 
-    <h3>Ziel</h3>
-    <p>Hacke werden</p>
+        <h2>Ziel</h2>
+        <p>Hacke werden.</p>
+    </div>
 
-    <h2>Ablauf einer Runde</h2>
-    <h3>Wette</h3>
+    <h1>Ablauf einer Runde</h1>
+    <h2>1. Wette</h2>
     <p>Reihe um wählt jeder Spieler einen Zug und die dazugehörigen Wetten aus.</p>
     <p>Insgesamt stehen 5 Züge zur Verfügung, die aktuell am gewählten Bahnhof ankommen sollten.</p>
     <p>Für den Zug wird auf Ereignisse wie “Verspätung aus vorheriger Fahrt”, “Polizeiliche Ermittlung” oder
@@ -30,7 +30,7 @@
     <p>Genauso gibt es eine festgelegte Anzahl an Schlücken die man trinken muss, wenn das Ereignis nicht
         eintritt.</p>
 
-    <h3>Auswertung der Wette</h3>
+    <h2>2. Auswertung der Wette</h2>
     <p>Für jeden Spieler wird die Anzahl an Schlücken die er verteilen darf oder trinken muss wie folgt
         berechnet:</p>
     <p>Schluck-Verteilanzahl richtige gewettete Ereignisse – Schluck-Trinkanzahl falsch gewettete Ereignisse =
@@ -38,10 +38,10 @@
     <p>Negative Schluckzahl -&gt; Schlücke trinken</p>
     <p>Positive Schluckzahl -&gt; Schlücke verteilen</p>
 
-    <h3>Strafen</h3>
-    <p>Wer Verspätungen googelt muss ein Bier trichtern</p>
+    <h2>Strafen</h2>
+    <p>Wer Verspätungen googelt muss ein Bier trichtern.</p>
 
-    <h2>Wie kommen die Züge und Verspätungsgründe zustande?</h2>
+    <h1>Wie kommen die Züge und Verspätungsgründe zustande?</h1>
     <p>Beim Rundenstart werden alle Züge, die am gewählten Bahnhof zur aktuellen Zeit ankommen sollten, von der
         Deutschen Bahn angefragt. Aus diesen Zügen werden dann fünf Zufällige gewählt.</p>
     <p>Dazu werden die zugehörigen Verspätungsgründe der Züge gespeichert.</p>
@@ -49,6 +49,12 @@
     <p>Es wird zusätzlich dafür gesorgt, dass wenn innerhalb kurzer Zeit mehrere Runde gespielt werden, dieselben
         Züge nicht erneut auftreten können.</p>
 
-    <h2>Spielende</h2>
-    <p>Wenn Hacke</p>
+    <h1>Spielende</h1>
+    <p>Wenn Hacke.</p>
 </div>
+
+<style>
+    h1 {
+        margin-top: 42px;
+    }
+</style>
