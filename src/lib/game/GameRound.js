1052ed232eb09793856656c2c989d4f0d72956fe
NAP-Programing <120453847+incaner@users.noreply.github.com>
2024-05-24 17:45:43 +0200

    Update GameRound.js
    Gerades runden du Sack

--- src/lib/game/GameRound.js
+++ src/lib/game/GameRound.js
@@ -105,7 +105,8 @@ export class GameRound {
             } else {
                 betsResult.push(({id: betCode, isCorrect: false}))
                 if(bet){
-                 totalSips -= (bet.sips / 2);
+                 totalSips -= Math.ceil(bet.sips / 2);
+                    
                 }
             }
         }
