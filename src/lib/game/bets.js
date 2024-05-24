7a059d356ed11c6c662e68d363011fbeeef49618
NAP-Programing <120453847+incaner@users.noreply.github.com>
2024-05-24 17:41:42 +0200

    Update bets.js
    Higher sips

--- src/lib/game/bets.js
+++ src/lib/game/bets.js
@@ -12,71 +12,71 @@ export const bets = [
     {
         code: 2,
         message: "Polizeiliche Ermittlung",
-        sips: 3
+        sips: 5
     },
     {
         code: 4,
         message: "Kurzfristiger Personalausfall",
-        sips: 2
+        sips: 4
     },
     {
         code: 6,
         message: "Betätigen der Notbremse",
-        sips: 10
+        sips: 16
     },
     {
         code: 7,
         message: "Personen im Gleis",
-        sips: 4
+        sips: 8
     },
     {
         code: 8,
         message: "Notarzteinsatz am Gleis",
-        sips: 3
+        sips: 10
     },
     {
         code: 16,
         message: "Entschärfung einer Fliegerbombe",
-        sips: 100
+        sips: 420
     },
     {
         code: 31,
         message: "Bauarbeiten",
-        sips: 2
+        sips: 4
     },
     {
         code: 36,
         message: "Technische Störung am Zug",
-        sips: 3
+        sips: 6
     },
     {
         code: 43,
         message: "Verspätung eines vorausfahrenden Zuges",
-        sips: 2
+        sips: 6
     },
     {
         code: 44,
         message: "Warten auf einen entgegenkommenden Zug",
-        sips: 2
+        sips: 8
     },
     {
         code: 47,
         message: "Verspätete Bereitstellung des Zuges",
-        sips: 3
+        sips: 8
     },
     {
         code: 48,
         message: "Verspätung aus vorheriger Fahrt",
-        sips: 2
+        sips: 6
     },
     {
         code: 80,
         message: "Andere Reihenfolge der Wagen",
-        sips: 2
+        sips: 4
     },
     {
         code: 82,
         message: "Mehrere Wagen fehlen",
-        sips: 3
+        sips: 10
     },
 ];
